cc6c2c448a62c3af2870dc287de0213ef4bcfa9d
刘永涛 <>
2021-02-22 18:14:10 +0800

    add mybatis plus

---
+++ src/test/java/com/hello/mybatisplus3/Mybatisplus3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.mybatisplus3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mybatisplus3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
